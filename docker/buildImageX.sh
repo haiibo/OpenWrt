daf3ee001b93315077b0fc920fa0ca6a59803b7d
Summer <85640068+haiibo@users.noreply.github.com>
2022-08-31 15:43:21 +0800

    优化 docker 打包方式

--- docker/buildImageX.sh
+++ docker/buildImageX.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-TAG=latest
-if [ ! -z "$1" ];then
-	TAG=$1
-fi
-
-TMPDIR=openwrt_rootfs
-OUTDIR=dockerimgs/docker
-IMG_NAME=summary/openwrt-aarch64
+TMPDIR=openwrt-armvirt
 
 [ -d "$TMPDIR" ] && rm -rf "$TMPDIR"
 
 mkdir -p "$TMPDIR"  && \
-mkdir -p "$OUTDIR"  && \
 gzip -dc openwrt-armvirt-64-default-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
 cp -f patches/rc.local "$TMPDIR/etc/" && \
 cp -f patches/cpustat "$TMPDIR/usr/bin/" && chmod 755 "$TMPDIR/usr/bin/cpustat" && \
@@ -27,9 +19,4 @@ sed -e "s/:0:0:99999:7:::/:${ddd}:0:99999:7:::/" -i "${TMPDIR}/etc/shadow" && \
 echo "17 3 * * * /etc/coremark.sh" >> "$TMPDIR/etc/crontabs/root" && \
 rm -rf "$TMPDIR/lib/firmware/*" "$TMPDIR/lib/modules/*" && \
 (cd "$TMPDIR" && tar cf ../openwrt-armvirt-64-default-rootfs-patched.tar .) && \
-rm -f DockerImg-OpenwrtArm64-${TAG}.gz && \
-# docker build -t ${IMG_NAME}:${TAG} . && \
-docker buildx build --platform=linux/arm64 -o type=docker -t ${IMG_NAME}:${TAG} . && \
-rm -f  openwrt-armvirt-64-default-rootfs-patched.tar && \
-rm -rf "$TMPDIR" && \
-docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-aarch64-${TAG}.gz
+rm -rf "$TMPDIR"
