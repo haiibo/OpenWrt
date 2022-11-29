5dc9f20a29a2a495aeaed9a1a47cb8b9fdd928ce
Summer <85640068+haiibo@users.noreply.github.com>
2022-11-29 12:00:29 +0800

    Add application store

--- diy-script.sh
+++ diy-script.sh
@@ -102,9 +102,13 @@ svn co https://github.com/haiibo/packages/trunk/wrtbwmon package/wrtbwmon
 svn co https://github.com/sbwml/luci-app-alist/trunk/luci-app-alist package/luci-app-alist
 svn co https://github.com/sbwml/luci-app-alist/trunk/alist package/alist
 
+# iStore
+svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
+svn co https://github.com/linkease/istore/trunk/luci package/istore
+
 # 设置向导
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-wizard package/luci-app-wizard
-sed -i 's/"admin"/"admin", "system"/g' package/luci-app-wizard/files/luci/controller/wizard.lua
+sed -i 's/"admin"/"admin", "system"/g' package/luci-app-wizard/luasrc/controller/wizard.lua
 
 # 在线用户
 svn co https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
