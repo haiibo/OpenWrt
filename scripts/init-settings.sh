432c32c1f9eea0c062fc1445c31015426c7727ab
Summer <85640068+haiibo@users.noreply.github.com>
2022-12-01 17:14:33 +0800

    Cancel boot files check

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -17,7 +17,7 @@ sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
 # Check file system during boot
-uci set fstab.@global[0].check_fs=1
-uci commit fstab
+# uci set fstab.@global[0].check_fs=1
+# uci commit fstab
 
 exit 0
