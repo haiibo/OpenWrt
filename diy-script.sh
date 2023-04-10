d80d350c85861f2d338489e06fac15f398907ae4
haiibo <boyhaibo@gmail.com>
2023-04-11 00:57:37 +0800

    Add gn for naiveproxy

--- diy-script.sh
+++ diy-script.sh
@@ -67,6 +67,7 @@ svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/xray-plugin
 svn co https://github.com/fw876/helloworld/trunk/lua-neturl package/lua-neturl
 svn co https://github.com/fw876/helloworld/trunk/trojan package/trojan
 svn co https://github.com/fw876/helloworld/trunk/redsocks2 package/redsocks2
+svn co https://github.com/fw876/helloworld/trunk/gn package/gn
 
 # Themes
 git clone --depth 1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
