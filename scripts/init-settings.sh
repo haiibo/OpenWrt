dd880800a34b9c0dd308cc162c1c18d97680c602
haiibo <boyhaibo@gmail.com>
2023-11-08 20:09:50 +0800

    Preset adguardhome core

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-#===============================================
-# File name: init-settings.sh
-# Description: This script will be executed during the first boot
-# Author: SuLingGG
-# Blog: https://mlapp.cn
-#===============================================
 
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit luci
-
-# Disable opkg signature check
-sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 
 # Disable IPV6 ula prefix
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
