e20135ea6068d0840f94c94a23c85688df4d74a3
久川 <82786875+jiu-chuan@users.noreply.github.com>
2023-12-06 17:27:23 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -10,7 +10,7 @@ sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_genera
 sed -i 's/auto//' package/base-files/files/bin/config_generate
 
 # 端口初始化
-cp -f $GITHUB_WORKSPACE/scripts/99-default_network.txt package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/scripts/99-default_network.txt package/base-files/files/etc/board.d/99-default_network
 
 # TTYD 自动登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
