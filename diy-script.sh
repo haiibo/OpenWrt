77fae203ba11f7e3b5332fac3f26a309134ab9f0
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2024-01-15 14:56:59 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -38,6 +38,7 @@ git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-quickstart quickstart
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
