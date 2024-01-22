4321e77280c824d92182b2ea4a89863ff8f68cc9
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2024-01-22 21:31:20 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -39,7 +39,7 @@ git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 git clone --depth=1 https://github.com/Erope/openwrt_nezha package/openwrt_nezha
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-advanced luci-app-netspeedtest luci-app-quickstart quickstart homebox
+git_sparse_clone main https://github.com/kenzok8/small-package luci-app-advanced luci-app-fileassistant luci-app-netspeedtest luci-app-quickstart quickstart homebox
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
