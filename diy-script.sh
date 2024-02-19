322ab6c9765af308482f5abf1b33463eff382f78
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2024-02-19 13:31:50 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -117,7 +117,7 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
-sed -i 's/iperf3-ssl[[:space:]]*//g' openwrt/target/linux/x86/Makefile
+sed -i 's/iperf3-ssl[[:space:]]*//g' target/linux/x86/Makefile
 
 # 取消主题默认设置
 find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
