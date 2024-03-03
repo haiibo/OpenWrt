942a379ecb8ba6c1a54070625bda4c852d2a6955
tonylee2022 <109421105+tonylee2022@users.noreply.github.com>
2024-03-04 01:08:39 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -114,8 +114,8 @@ cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/
 sed -i 's/iperf3-ssl/iperf3/g' package/luci-app-netspeedtest/Makefile
 
 # 修复 passwall passwall2 百度https错误
-sed -i 's/‘https://www.baidu.com’/‘http://www.baidu.com’/g' package/luci-app-passwall/luasrc/view/passwall/global/status.htm
-sed -i 's/‘https://www.baidu.com’/‘http://www.baidu.com’/g' package/luci-app-passwall2/luasrc/view/passwall2/global/status.htm
+sed -i 's/‘https://www.baidu.com’/‘http://www.baidu.com’/g' package/luci-app-passwall/luci-app-passwall/luasrc/view/passwall/global/status.htm
+sed -i 's/‘https://www.baidu.com’/‘http://www.baidu.com’/g' package/luci-app-passwall2/luci-app-passwall2/luasrc/view/passwall2/global/status.htm
 
 # 修改 cloudflared 版本 测试
 # sed -i 's/2023.2.2/2024.2.1/g' feeds/packages/net/cloudflared/Makefile
